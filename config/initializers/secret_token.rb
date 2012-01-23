75b49f9391a3634df2f995feb16fb12b4f3e7192
Ashish Virmani <virmani.ashish@gmail.com>
2012-01-22 17:18:14 -0800

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SugarMint::Application.config.secret_token = '4f40d9516059e0b5d5f8a753ad27393e02b53458e92d57cd354244b4663a1836c16a3653abaa8ddcf052b087ffcff0faf783e0248cf88de301fa4dbbd3e54b04'
